01d33a885e98ef66ebefa5fc32806ee31d442377
Nivaldo Martinez <nivaldomartinez@Nivaldos-MacBook-Pro.local>
2020-07-31 10:55:12 -0500

    feat: added origin *

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const bodyParser = require('body-parser');
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.ORIGIN_ALLOWED || '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 
 const port = process.env.PORT || 3000
