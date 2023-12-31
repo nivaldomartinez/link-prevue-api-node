79bef6d7e5ed54c4ffa5c4664370a7501724fe47
Nivaldo <nivaldo.developer@gmail.com>
2023-12-31 12:36:55 -0500

    link preview prvw version

--- src/link-preview.ts
+++ src/link-preview.ts
@@ -45,17 +45,12 @@ function collectMeta($: cheerio.CheerioAPI, url: string) {
 const linkPreview = async (url: string) => {
     if (!url || url === '') return Promise.reject({ message: 'You must add a valid url' });
     if (!url.match(/^http(s)?:\/\/[a-z]+\.[a-z]+(.)+/i)) return Promise.resolve(getEmptyData(url));
-
-    const headers = new Headers()
-    headers.append('Content-Type', 'text/html; charset=UTF-8')
   
-    const response = await fetch(url, { headers })
+    const response = await fetch(url)
     if (!response) return getEmptyData(url);
     if (response.status === 200) {
-        const buffer = await response.arrayBuffer()
-        const decoder = new TextDecoder('iso-8859-1');
-        const body = decoder.decode(buffer);
-        return collectMeta(cheerio.load(body, { decodeEntities: true }), url)
+        const body = await response.text()
+        return collectMeta(cheerio.load(body), url)
     }
     return getEmptyData(url);
   }
